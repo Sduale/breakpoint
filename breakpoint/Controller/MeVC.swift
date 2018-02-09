2326af61691557ec29a6e6368e005c58e48763f3
Sharmarke Duale <sdua1301@gmail.com>
2018-02-09 09:45:59 -0600

    add UI for FeedVC, GroupsVC and MeVC

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,30 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by nopcallowed on 2/9/18.
+//  Copyright © 2018 GASHAN INC. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func signOutBtnWasPrssd(_ sender: Any) {
+        // Sign out code..
+        
+    }
+    
+
+}
